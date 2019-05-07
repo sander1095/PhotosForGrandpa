1c4f98f3ebfaa3d3b46b66ca5890dc7afc07b9b0
Sander ten Brinke <sander.ten.brinke@xs4all.nl>
2019-05-07 19:04:57 +0200

    Created empty project

---
+++ PhotosForGrandpa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhotosForGrandpa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
