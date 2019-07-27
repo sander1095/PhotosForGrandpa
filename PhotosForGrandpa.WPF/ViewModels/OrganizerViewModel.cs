7a08733698645928854653bd19064f1e130914d0
Sander ten Brinke <s.tenbrinke@arcady.nl>
2019-07-27 13:06:48 +0200

    Now unzipping files

--- PhotosForGrandpa.WPF/ViewModels/OrganizerViewModel.cs
+++ PhotosForGrandpa.WPF/ViewModels/OrganizerViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using Mecha.ViewModel.Attributes;
 using PhotosForGrandpa.WPF.Exceptions;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 
 namespace PhotosForGrandpa.WPF.ViewModels
 {
     public class OrganizerViewModel
     {
+        private const string GooglePhotosDownloadFileName = "Photos.zip";
+
         [Readonly]
-        public virtual string Intro => 
+        public virtual string Intro =>
             $"Dit programma zal ervoor zorgen dat gedownloade foto's van uw {Environment.NewLine}" +
             $"telefoon automatisch gesorteerd worden in de Afbeeldingen {Environment.NewLine}" +
             $"en Video map!{Environment.NewLine}" +
@@ -18,7 +22,7 @@ namespace PhotosForGrandpa.WPF.ViewModels
             $"Stap 3: Kies een goede naam voor de map die gemaakt {Environment.NewLine}" +
             $"Stap 4: Klik op de Organiseer knop";
 
-        [TextInput("Mapnaam", Description = "Gran Canaria 2019", Mandatory =true)]
+        [TextInput("Mapnaam", Description = "Gran Canaria 2019", Mandatory = true)]
         public virtual string FolderName { get; set; }
 
         [Action]
@@ -28,8 +32,15 @@ namespace PhotosForGrandpa.WPF.ViewModels
             try
             {
                 Validate();
+
+                var filesInArchive = UnzipFile();
+
+                OrganizePhotos(filesInArchive);
+                OrganizeVideos(filesInArchive);
+
+                Cleanup();
             }
-            catch(Exception e) when(!(e is AppException))
+            catch (Exception e) when (!(e is AppException))
             {
                 //TODO: Log error
 
@@ -38,7 +49,7 @@ namespace PhotosForGrandpa.WPF.ViewModels
                     $"Nodig uw kleinzoon uit voor rijstepap en hij zal het oplossen!");
             }
         }
-       
+
         private void Validate()
         {
             var newImagesFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), FolderName);
@@ -50,16 +61,50 @@ namespace PhotosForGrandpa.WPF.ViewModels
                 _ = Path.GetFullPath(newImagesFolder);
                 _ = Path.GetFullPath(newVideosFolder);
 
-                if(FolderName.Contains("/") || FolderName.Contains("\\"))
+                if (FolderName.Contains("/") || FolderName.Contains("\\"))
                 {
                     throw new AppException();
                 }
             }
             catch
             {
-                throw new AppException("De Mapnaam mag de volgende karakters niet bevatten: \\ / : * ? \" < > |");
+                throw new AppException("De mapnaam mag de volgende karakters niet bevatten: \\ / : * ? \" < > |");
             }
         }
-    }
 
+        private IEnumerable<ZipArchiveEntry> UnzipFile()
+        {
+            var expectedZipFilePath = Path.Combine(Syroot.Windows.IO.KnownFolders.DownloadsLocalized.Path, GooglePhotosDownloadFileName);
+
+            // Ensures that the last character on the extraction path
+            // is the directory separator char. 
+            // Without this, a malicious zip file could try to traverse outside of the expected
+            // extraction path.
+            if (!expectedZipFilePath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+            {
+                expectedZipFilePath += Path.DirectorySeparatorChar;
+            }
+
+            using (ZipArchive archive = ZipFile.OpenRead(expectedZipFilePath))
+            {
+                return archive.Entries;
+            }
+        }
+
+        private void OrganizePhotos(IEnumerable<ZipArchiveEntry> filesInArchive)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OrganizeVideos(IEnumerable<ZipArchiveEntry> filesInArchive)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Cleanup()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
